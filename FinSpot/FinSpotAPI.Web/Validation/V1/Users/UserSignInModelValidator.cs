e87f117ff0900ce6af5f396c47c756803b4a63c3
Raman Zinevich <raman.zinevich@innowise.com>
2024-08-23 13:10:06 +0300

    Additional validation.

---
+++ FinSpot/FinSpotAPI.Web/Validation/V1/Users/UserSignInModelValidator.cs
@@ -0,0 +1,23 @@
+﻿using FinSpotAPI.Common.Constants;
+using FinSpotAPI.Web.Models.V1.Users.Inbound;
+using FluentValidation;
+
+namespace FinSpotAPI.Web.Validation.V1.Users
+{
+    public class UserSignInModelValidator : AbstractValidator<UserSignInModel>
+    {
+        public UserSignInModelValidator()
+        {
+            RuleFor(_ => _.Email)
+                .NotEmpty()
+                .NotNull()
+                .EmailAddress()
+                .MaximumLength(ModelValidationConstants.Values.EmailMaxLength);
+
+            RuleFor(_ => _.Password)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(ModelValidationConstants.Values.PasswordMaxLength);
+        }
+    }
+}
